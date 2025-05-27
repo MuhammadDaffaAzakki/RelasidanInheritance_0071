611b78c21aafc4da4a0b9bc55f4200ae90b6bc9f
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-05-27 16:05:09 +0700

    membuat void pasien

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -35,3 +35,17 @@ class dokter {
         void tambahPasien(pasien*);
         void cetakPasien();
 };
+
+void pasien::tambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+}
+
+void pasien::cetakDokter() {
+    cout << "Daftar dokter yang menangani pasien \""
+    << this->nama << "\":\n";
+    //auto digunakan dalam perulangan
+    for (auto& a : daftar_dokter) {
+        cout << a-> nama << "\n";
+    }
+    cout << endl;
+}
